2b4f2a0db20aa87391d5bda7c8456acde5006241
Ancelad <i_am_pomidorchik@mail.ru>
2016-09-05 23:48:33 +0600

    ts-files

---
+++ translations/sailfishos-mazelock-patch-ru.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message id="MazeLock settings">
+        <location filename="main.qml" line="27"/>
+        <source>MazeLock settings</source>
+        <translation>Настройки MazeLock</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="32"/>
+        <source>Mask input character immediately</source>
+        <translation>Скрывать вводимые символы немедленно</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="39"/>
+        <source>Draw colored lines between nodes</source>
+        <translation>Отрисовывать цветные линии между точками</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="46"/>
+        <source>MazeLock size</source>
+        <translation>Размер MazeLock</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/sailfishos-mazelock-patch.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="MazeLock settings">
+        <location filename="main.qml" line="27"/>
+        <source>MazeLock settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="32"/>
+        <source>Mask input character immediately</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="39"/>
+        <source>Draw colored lines between nodes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="46"/>
+        <source>MazeLock size</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
